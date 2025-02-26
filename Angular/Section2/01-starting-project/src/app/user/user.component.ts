476a0f88dec72c20e0173fbb59f91470141731af
Tristan Bookhan <tristanbookhan@gmail.com>
2025-02-26 21:51:11 +0200

    Getters, Event listeners and state and changing data

--- Angular/Section2/01-starting-project/src/app/user/user.component.ts
+++ Angular/Section2/01-starting-project/src/app/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+let randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,5 +11,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar
+  }
+
+  onSelectUser() {
+
+    randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
+    
+  }
 }
