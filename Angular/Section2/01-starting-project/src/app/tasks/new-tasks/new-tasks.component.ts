2fd1d62bf9ea66648fa385091880b993f0035b93
Tristan Bookhan <tristanbookhan@gmail.com>
2025-03-23 18:27:30 +0200

    DIrectives and two-way-binding

--- Angular/Section2/01-starting-project/src/app/tasks/new-tasks/new-tasks.component.ts
+++ Angular/Section2/01-starting-project/src/app/tasks/new-tasks/new-tasks.component.ts
@@ -1,15 +1,19 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-tasks',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-tasks.component.html',
   styleUrl: './new-tasks.component.css'
 })
 export class NewTasksComponent {
 
   @Output() cancelNewTask = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
 
   onCancelNewTask() {
     this.cancelNewTask.emit();
