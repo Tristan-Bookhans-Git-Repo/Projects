f6abc66aac8ad8afd046020a7f528319f07c8f0b
Tristan Bookhan <tristanbookhan@gmail.com>
2025-04-13 14:12:39 +0200

    Getting started with dependency injection

--- Angular/Section2/01-starting-project/src/app/tasks/tasks.component.ts
+++ Angular/Section2/01-starting-project/src/app/tasks/tasks.component.ts
@@ -3,6 +3,8 @@ import { TaskComponent } from './task/task.component';
 import { NewTasksComponent } from "./new-tasks/new-tasks.component";
 import { type newTask } from './task/task.module';
 
+import { TasksService } from './tasks.service';
+
 @Component({
   selector: 'app-tasks',
   standalone: true,
@@ -17,38 +19,14 @@ export class TasksComponent {
   // @Output() addNewTask = new EventEmitter();
   isAddingTask = false;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    
   }
 
   onStartAddTask() {
@@ -62,13 +40,7 @@ export class TasksComponent {
 
   onAddNewTask(taskData: newTask) {
     this.isAddingTask = false;
-    this.tasks.push({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.dueDate
-    })
+    
   }
 
 }
